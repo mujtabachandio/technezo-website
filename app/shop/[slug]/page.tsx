c6856c1167d468c68d8966bf6fe7a6ae9c94a8f0
mujtaba chandio <mujtabachandio384@gmail.com>
2025-05-12 20:35:18 +0500

    update

--- app/shop/[slug]/page.tsx
+++ app/shop/[slug]/page.tsx
@@ -169,24 +169,6 @@ export default async function ProductPage({
 
                 </p>
               </div>
-
-              <div className="text-green-600 text-sm font-medium flex items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-5 h-5 mr-1"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                  />
-                </svg>
-                In Stock - Ready to Ship
-              </div>
             </div>
 
             <div className="border-t border-b border-gray-200 py-6">
@@ -194,10 +176,11 @@ export default async function ProductPage({
                 {[
                   ["Brand", product.brand],
                   ["Processor", product.processor || "Not specified"],
-                  ["GPU", product.gpu],
+                  ["Gen", product.generation],
                   ["RAM", product.ram],
                   ["Storage", product.storage || "Not specified"],
                   ["Screen Size", product.screenSize || "Not specified"],
+                  ["GPU", product.gpu],
                   ["OS", product.os || "Not specified"],
                   ["Warranty", product.warranty || "None"],
                 ].map(([label, value], idx) => (
